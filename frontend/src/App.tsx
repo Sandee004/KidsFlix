05dde432be5b8535500ebaee6227035bf54735e3
Sandra <oyarekhuas@gmail.com>
2024-07-31 10:32:56 +0100

    Fixed build error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,19 @@ import LoginPage from "./components/login";
 import RegisterPage from "./components/register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+const handleAddFavourite = () => {
+    console.log("Movie list updated:");
+};
+
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<FetchComponent />} />
-                <Route path="/movies/:id" element={<MovieDetail />} />
+                <Route
+                    path="/movies/:id"
+                    element={<MovieDetail addFavourite={handleAddFavourite} />}
+                />
                 <Route path="/favourites" element={<FavouritesPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
